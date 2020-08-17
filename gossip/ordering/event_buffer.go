08999a577c466a956c5ddb493b0dce1de5e36292
Egor Lysenko <uprendis@gmail.com>
2020-08-17 13:14:18 +0700

    decouple events ordering buffer from Opera chain

--- gossip/ordering/event_buffer.go
+++ gossip/ordering/event_buffer.go
@@ -5,24 +5,24 @@ import (
 
 	"github.com/Fantom-foundation/go-lachesis/eventcheck"
 	"github.com/Fantom-foundation/go-lachesis/hash"
-	"github.com/Fantom-foundation/go-lachesis/inter"
+	"github.com/Fantom-foundation/go-lachesis/inter/dag"
 )
 
 type (
 	// event is a inter.Event and data for ordering purpose.
 	event struct {
-		*inter.Event
+		dag.Event
 
 		peer string
 	}
 
 	// Callback is a set of EventBuffer()'s args.
 	Callback struct {
-		Process func(e *inter.Event) error
-		Drop    func(e *inter.Event, peer string, err error)
-		Get     func(hash.Event) *inter.EventHeaderData
+		Process func(e dag.Event) error
+		Drop    func(e dag.Event, peer string, err error)
+		Get     func(hash.Event) dag.Event
 		Exists  func(hash.Event) bool
-		Check   func(e *inter.Event, parents []*inter.EventHeaderData) error
+		Check   func(e dag.Event, parents []dag.Event) error
 	}
 )
 
@@ -39,7 +39,7 @@ func New(buffSize int, callback Callback) *EventBuffer {
 	}
 }
 
-func (buf *EventBuffer) PushEvent(e *inter.Event, peer string) {
+func (buf *EventBuffer) PushEvent(e dag.Event, peer string) {
 	w := &event{
 		Event: e,
 		peer:  peer,
@@ -62,19 +62,19 @@ func (buf *EventBuffer) getIncompleteEventsList() []*event {
 
 func (buf *EventBuffer) pushEvent(e *event, incompleteEventsList []*event, strict bool) {
 	// LRU is thread-safe, no need in mutex
-	if buf.callback.Exists(e.Hash()) {
+	if buf.callback.Exists(e.ID()) {
 		if strict {
 			buf.callback.Drop(e.Event, e.peer, eventcheck.ErrAlreadyConnectedEvent)
 		}
 		return
 	}
 
-	parents := make([]*inter.EventHeaderData, len(e.Parents)) // use local buffer for thread safety
-	for i, p := range e.Parents {
+	parents := make([]dag.Event, len(e.Parents())) // use local buffer for thread safety
+	for i, p := range e.Parents() {
 		_, _ = buf.incompletes.Get(p) // updating the "recently used"-ness of the key
 		parent := buf.callback.Get(p)
 		if parent == nil {
-			buf.incompletes.Add(e.Hash(), e)
+			buf.incompletes.Add(e.ID(), e)
 			return
 		}
 		parents[i] = parent
@@ -97,10 +97,10 @@ func (buf *EventBuffer) pushEvent(e *event, incompleteEventsList []*event, stric
 	}
 
 	// now child events may become complete, check it again
-	eHash := e.Hash()
+	eHash := e.ID()
 	buf.incompletes.Remove(eHash)
 	for _, child := range incompleteEventsList {
-		for _, parent := range child.Parents {
+		for _, parent := range child.Parents() {
 			if parent == eHash {
 				buf.pushEvent(child, incompleteEventsList, false)
 			}
--- gossip/ordering/ordering_test.go
+++ gossip/ordering/ordering_test.go
@@ -1,51 +1,53 @@
 package ordering
 
 import (
+	"errors"
 	"math/rand"
 	"testing"
 	"time"
 
-	"github.com/Fantom-foundation/go-lachesis/eventcheck/parentscheck"
 	"github.com/Fantom-foundation/go-lachesis/hash"
-	"github.com/Fantom-foundation/go-lachesis/inter"
-	"github.com/Fantom-foundation/go-lachesis/lachesis"
+	"github.com/Fantom-foundation/go-lachesis/inter/dag"
+	"github.com/Fantom-foundation/go-lachesis/inter/dag/tdag"
 )
 
 func TestEventBuffer(t *testing.T) {
-	nodes := inter.GenNodes(5)
+	nodes := tdag.GenNodes(5)
 
-	var ordered []*inter.Event
+	var ordered []dag.Event
 	r := rand.New(rand.NewSource(time.Now().Unix()))
-	_ = inter.ForEachRandEvent(nodes, 10, 3, r, inter.ForEachEvent{
-		Process: func(e *inter.Event, name string) {
+	_ = tdag.ForEachRandEvent(nodes, 10, 3, r, tdag.ForEachEvent{
+		Process: func(e dag.Event, name string) {
 			ordered = append(ordered, e)
 		},
-		Build: func(e *inter.Event, name string) *inter.Event {
-			e.Epoch = 1
-			e.ClaimedTime = inter.Timestamp(e.Seq)
-			return e
+		Build: func(e dag.MutableEvent, name string) error {
+			e.SetEpoch(1)
+			e.SetRawTime(dag.RawTimestamp(e.Seq()))
+			return nil
 		},
 	})
 
-	processed := make(map[hash.Event]*inter.EventHeaderData)
+	checked := 0
+
+	processed := make(map[hash.Event]dag.Event)
 	buffer := New(len(nodes)*10, Callback{
 
-		Process: func(e *inter.Event) error {
-			if _, ok := processed[e.Hash()]; ok {
+		Process: func(e dag.Event) error {
+			if _, ok := processed[e.ID()]; ok {
 				t.Fatalf("%s already processed", e.String())
 				return nil
 			}
-			for _, p := range e.Parents {
+			for _, p := range e.Parents() {
 				if _, ok := processed[p]; !ok {
 					t.Fatalf("got %s before parent %s", e.String(), p.String())
 					return nil
 				}
 			}
-			processed[e.Hash()] = &e.EventHeaderData
+			processed[e.ID()] = e
 			return nil
 		},
 
-		Drop: func(e *inter.Event, peer string, err error) {
+		Drop: func(e dag.Event, peer string, err error) {
 			t.Fatalf("%s unexpectedly dropped with %s", e.String(), err)
 		},
 
@@ -53,11 +55,17 @@ func TestEventBuffer(t *testing.T) {
 			return processed[e] != nil
 		},
 
-		Get: func(e hash.Event) *inter.EventHeaderData {
+		Get: func(e hash.Event) dag.Event {
 			return processed[e]
 		},
 
-		Check: parentscheck.New(&lachesis.DagConfig{}).Validate,
+		Check: func(e dag.Event, parents []dag.Event) error {
+			checked++
+			if e.RawTime() != dag.RawTimestamp(e.Seq()) {
+				return errors.New("malformed event time")
+			}
+			return nil
+		},
 	})
 
 	for _, rnd := range rand.Perm(len(ordered)) {
@@ -67,8 +75,11 @@ func TestEventBuffer(t *testing.T) {
 
 	// everything is processed
 	for _, e := range ordered {
-		if _, ok := processed[e.Hash()]; !ok {
+		if _, ok := processed[e.ID()]; !ok {
 			t.Fatal("event wasn't processed")
 		}
+	}
+	if checked != len(processed) {
+		t.Fatal("not all the events were checked")
 	}
 }
