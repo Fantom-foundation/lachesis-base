1aaafe7e58d1e758b847370a572c63bb89831225
Egor Lysenko <uprendis@gmail.com>
2020-08-19 17:04:52 +0700

    rm unused

--- eventcheck/ban.go
+++ eventcheck/ban.go
@@ -2,18 +2,8 @@ package eventcheck
 
 import (
 	"errors"
-
-	"github.com/Fantom-foundation/lachesis-base/eventcheck/epochcheck"
 )
 
 var (
 	ErrAlreadyConnectedEvent = errors.New("event is connected already")
 )
-
-func IsBan(err error) bool {
-	if err == epochcheck.ErrNotRelevant ||
-		err == ErrAlreadyConnectedEvent {
-		return false
-	}
-	return err != nil
-}
--- inter/idx/internal.go
+++ inter/idx/internal.go
@@ -18,10 +18,3 @@ func (v Validator) Bytes() []byte {
 func BytesToValidator(b []byte) Validator {
 	return Validator(bigendian.BytesToUint32(b))
 }
-
-const (
-	// ShortTermGas is short-window settings for gas power
-	ShortTermGas = 0
-	// LongTermGas is long-window settings for gas power
-	LongTermGas = 1
-)
