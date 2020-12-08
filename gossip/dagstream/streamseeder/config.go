61629d1ffa898f31ccd90f78e1afeb07f156ed84
Egor Lysenko <uprendis@gmail.com>
2020-12-08 12:06:20 +0700

    streamseeder: limit RAM usage, session responses in order of requests

--- gossip/dagstream/streamseeder/config.go
+++ gossip/dagstream/streamseeder/config.go
@@ -1,11 +1,15 @@
 package streamseeder
 
 type Config struct {
-	SenderThreads int
+	SenderThreads           int
+	MaxMaxSenderTasks       int
+	MaxPendingResponsesSize int32
 }
 
 func DefaultConfig() Config {
 	return Config{
-		SenderThreads: 1,
+		SenderThreads:           8,
+		MaxPendingResponsesSize: 64 * 1024 * 1024,
+		MaxMaxSenderTasks:       128,
 	}
 }
