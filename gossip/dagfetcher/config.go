ee50d84f5e13a4a5fe152cda375c3c71ad7c3ae3
Egor Lysenko <uprendis@gmail.com>
2020-12-08 11:51:55 +0700

    dagfetcher: refactor, erase events processing

--- gossip/dagfetcher/config.go
+++ gossip/dagfetcher/config.go
@@ -3,33 +3,28 @@ package dagfetcher
 import "time"
 
 type Config struct {
-	ForgetTimeout time.Duration // RawTime before an announced event is forgotten
-	ArriveTimeout time.Duration // RawTime allowance before an announced event is explicitly requested
+	ForgetTimeout time.Duration // Time before an announced event is forgotten
+	ArriveTimeout time.Duration // Time allowance before an announced event is explicitly requested
 	GatherSlack   time.Duration // Interval used to collate almost-expired announces with fetches
-	FetchTimeout  time.Duration // Maximum allowed time to return an explicitly requested event
 	HashLimit     int           // Maximum number of unique events a peer may have announced
 
-	MaxEventsBatch int // Maximum number of events in an inject batch (batch is divided if exceeded)
-	MaxHashesBatch int // Maximum number of hashes in an announce batch (batch is divided if exceeded)
+	MaxBatch int // Maximum number of hashes in an announce batch (batch is divided if exceeded)
 
-	// MaxQueuedEventsBatches is the maximum number of inject batches to queue up before
-	// dropping incoming events.
-	MaxQueuedEventsBatches int
+	MaxParallelRequests int // Maximum number of parallel requests
+
 	// MaxQueuedHashesBatches is the maximum number of announce batches to queue up before
 	// dropping incoming hashes.
-	MaxQueuedHashesBatches int
+	MaxQueuedBatches int
 }
 
 func DefaultConfig() Config {
 	return Config{
-		ForgetTimeout:          1 * time.Minute,
-		ArriveTimeout:          1000 * time.Millisecond,
-		GatherSlack:            100 * time.Millisecond,
-		FetchTimeout:           10 * time.Second,
-		HashLimit:              10000,
-		MaxEventsBatch:         8,
-		MaxHashesBatch:         256,
-		MaxQueuedEventsBatches: 128,
-		MaxQueuedHashesBatches: 128,
+		ForgetTimeout:       1 * time.Minute,
+		ArriveTimeout:       1000 * time.Millisecond,
+		GatherSlack:         100 * time.Millisecond,
+		HashLimit:           20000,
+		MaxBatch:            512,
+		MaxQueuedBatches:    32,
+		MaxParallelRequests: 256,
 	}
 }
