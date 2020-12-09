fcb722e545448336b127cd9dbcb2d2601e2245f2
alex <rusdev.alex@gmail.com>
2020-12-10 00:06:14 +1000

    readonly iterator

--- kvdb/flushable/readonly.go
+++ kvdb/flushable/readonly.go
@@ -39,16 +39,6 @@ func (ro *Readonly) Get(key []byte) ([]byte, error) {
 	return ro.underlying.Get(key)
 }
 
-// NewIterator creates a binary-alphabetical iterator over a subset
-// of database content with a particular key prefix, starting at a particular
-// initial key (or after, if it does not exist).
-func (ro *Readonly) NewIterator(prefix []byte, start []byte) kvdb.Iterator {
-	ro.mu.RLock()
-	defer ro.mu.RUnlock()
-	// NOTE: iterator's methods are not locked with ro.mu
-	return ro.underlying.NewIterator(prefix, start)
-}
-
 // Stat returns a particular internal stat of the database.
 func (ro *Readonly) Stat(property string) (string, error) {
 	ro.mu.RLock()
@@ -63,4 +53,61 @@ func (ro *Readonly) Compact(start []byte, limit []byte) error {
 	defer ro.mu.RUnlock()
 
 	return ro.underlying.Compact(start, limit)
+}
+
+// NewIterator creates a binary-alphabetical iterator over a subset
+// of database content with a particular key prefix, starting at a particular
+// initial key (or after, if it does not exist).
+func (ro *Readonly) NewIterator(prefix []byte, start []byte) kvdb.Iterator {
+	ro.mu.RLock()
+	defer ro.mu.RUnlock()
+
+	return &readonlyIterator{
+		mu:       ro.mu,
+		parentIt: ro.underlying.NewIterator(prefix, start),
+	}
+}
+
+/*
+ * Iterator
+ */
+
+type readonlyIterator struct {
+	mu       *sync.RWMutex
+	parentIt kvdb.Iterator
+}
+
+// Next scans key-value pair by key in lexicographic order. Looks in cache first,
+// then - in DB.
+func (it *readonlyIterator) Next() bool {
+	it.mu.RLock()
+	defer it.mu.RUnlock()
+
+	return it.parentIt.Next()
+}
+
+// Error returns any accumulated error. Exhausting all the key/value pairs
+// is not considered to be an error. A memory iterator cannot encounter errors.
+func (it *readonlyIterator) Error() error {
+	return it.parentIt.Error()
+}
+
+// Key returns the key of the current key/value pair, or nil if done. The caller
+// should not modify the contents of the returned slice, and its contents may
+// change on the next call to Next.
+func (it *readonlyIterator) Key() []byte {
+	return it.parentIt.Key()
+}
+
+// Value returns the value of the current key/value pair, or nil if done. The
+// caller should not modify the contents of the returned slice, and its contents
+// may change on the next call to Next.
+func (it *readonlyIterator) Value() []byte {
+	return it.parentIt.Value()
+}
+
+// Release releases associated resources. Release should always succeed and can
+// be called multiple times without causing error.
+func (it *readonlyIterator) Release() {
+	it.parentIt.Release()
 }
