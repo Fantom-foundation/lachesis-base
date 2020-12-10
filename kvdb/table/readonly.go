cd14b3140cab51e52770492017ffbf3f33ccbf06
alex <rusdev.alex@gmail.com>
2020-12-10 19:52:14 +1000

    kvdb/table.Readonly wrapper

--- kvdb/table/readonly.go
+++ kvdb/table/readonly.go
@@ -4,37 +4,65 @@ import (
 	"github.com/Fantom-foundation/lachesis-base/kvdb"
 )
 
-// readonly kvdb.Store panics on any data mutation.
-type readonly struct {
-	kvdb.ReadonlyStore
+// Readonly table wraper of the underling DB, so all the table's data is stored with a prefix in underling DB.
+type Readonly struct {
+	prefix     []byte
+	underlying kvdb.ReadonlyStore
 }
 
-// Put puts key-value pair into the cache.
-func (ro *readonly) Put(key []byte, value []byte) error {
-	panic("readonly!")
-	return nil
+func NewReadonly(db kvdb.ReadonlyStore, prefix []byte) *Readonly {
+	return &Readonly{
+		underlying: db,
+		prefix:     prefix,
+	}
 }
 
-// Delete removes key-value pair by key.
-func (ro *readonly) Delete(key []byte) error {
-	panic("readonly!")
-	return nil
+func (t *Readonly) NewReadonlyTable(prefix []byte) *Readonly {
+	return NewReadonly(t, prefix)
 }
 
-// Compact flattens the underlying data store for the given key range.
-func (ro *readonly) Compact(start []byte, limit []byte) error {
-	panic("readonly!")
-	return nil
+func (t *Readonly) Has(key []byte) (bool, error) {
+	return t.underlying.Has(prefixed(key, t.prefix))
 }
 
-// Close leaves underlying database.
-func (ro *readonly) Close() error {
-	panic("readonly!")
-	return nil
+func (t *Readonly) Get(key []byte) ([]byte, error) {
+	return t.underlying.Get(prefixed(key, t.prefix))
 }
 
-// NewBatch creates new batch.
-func (ro *readonly) NewBatch() kvdb.Batch {
-	panic("readonly!")
-	return nil
+func (t *Readonly) Stat(property string) (string, error) {
+	return t.underlying.Stat(property)
+}
+
+func (t *Readonly) NewIterator(itPrefix []byte, start []byte) kvdb.Iterator {
+	return &iterator{t.underlying.NewIterator(prefixed(itPrefix, t.prefix), start), t.prefix}
+}
+
+/*
+ * Iterator
+ */
+
+type iterator struct {
+	it     kvdb.Iterator
+	prefix []byte
+}
+
+func (it *iterator) Next() bool {
+	return it.it.Next()
+}
+
+func (it *iterator) Error() error {
+	return it.it.Error()
+}
+
+func (it *iterator) Key() []byte {
+	return noPrefix(it.it.Key(), it.prefix)
+}
+
+func (it *iterator) Value() []byte {
+	return it.it.Value()
+}
+
+func (it *iterator) Release() {
+	it.it.Release()
+	*it = iterator{}
 }
