72afa56d81368f21d781f06447ce3d04e7279b95
alex <rusdev.alex@gmail.com>
2020-12-15 02:59:24 +1000

    even empty utils/wlru.Cache works

--- utils/wlru/wlru.go
+++ utils/wlru/wlru.go
@@ -32,30 +32,42 @@ func NewWithEvict(maxWeight uint, maxSize int, onEvicted func(key interface{}, v
 
 // Purge is used to completely clear the cache.
 func (c *Cache) Purge() {
+	if c == nil {
+		return
+	}
 	c.lock.Lock()
 	c.lru.Purge()
 	c.lock.Unlock()
 }
 
-// Add adds a value to the cache. Returns true if an eviction occurred.
+// Add adds a value to the cache. Returns count of evictions occurred.
 func (c *Cache) Add(key, value interface{}, weight uint) (evicted int) {
+	if c == nil {
+		return
+	}
 	c.lock.Lock()
 	evicted = c.lru.Add(key, value, weight)
 	c.lock.Unlock()
-	return evicted
+	return
 }
 
 // Get looks up a key's value from the cache.
 func (c *Cache) Get(key interface{}) (value interface{}, ok bool) {
+	if c == nil {
+		return
+	}
 	c.lock.Lock()
 	value, ok = c.lru.Get(key)
 	c.lock.Unlock()
-	return value, ok
+	return
 }
 
 // Contains checks if a key is in the cache, without updating the
 // recent-ness or deleting it for being stale.
 func (c *Cache) Contains(key interface{}) bool {
+	if c == nil {
+		return false
+	}
 	c.lock.RLock()
 	containKey := c.lru.Contains(key)
 	c.lock.RUnlock()
@@ -65,44 +77,55 @@ func (c *Cache) Contains(key interface{}) bool {
 // Peek returns the key value (or undefined if not found) without updating
 // the "recently used"-ness of the key.
 func (c *Cache) Peek(key interface{}) (value interface{}, ok bool) {
+	if c == nil {
+		return
+	}
 	c.lock.RLock()
 	value, ok = c.lru.Peek(key)
 	c.lock.RUnlock()
-	return value, ok
+	return
 }
 
 // ContainsOrAdd checks if a key is in the cache without updating the
 // recent-ness or deleting it for being stale, and if not, adds the value.
 // Returns whether found and whether an eviction occurred.
 func (c *Cache) ContainsOrAdd(key, value interface{}, weight uint) (ok bool, evicted int) {
+	if c == nil {
+		return
+	}
 	c.lock.Lock()
 	defer c.lock.Unlock()
 
-	if c.lru.Contains(key) {
-		return true, 0
+	if ok = c.lru.Contains(key); ok {
+		return
 	}
 	evicted = c.lru.Add(key, value, weight)
-	return false, evicted
+	return
 }
 
 // PeekOrAdd checks if a key is in the cache without updating the
 // recent-ness or deleting it for being stale, and if not, adds the value.
 // Returns whether found and whether an eviction occurred.
 func (c *Cache) PeekOrAdd(key, value interface{}, weight uint) (previous interface{}, ok bool, evicted int) {
+	if c == nil {
+		return
+	}
 	c.lock.Lock()
 	defer c.lock.Unlock()
 
-	previous, ok = c.lru.Peek(key)
-	if ok {
-		return previous, true, 0
+	if previous, ok = c.lru.Peek(key); ok {
+		return
 	}
 
 	evicted = c.lru.Add(key, value, weight)
-	return nil, false, evicted
+	return
 }
 
 // Remove removes the provided key from the cache.
 func (c *Cache) Remove(key interface{}) (present bool) {
+	if c == nil {
+		return
+	}
 	c.lock.Lock()
 	present = c.lru.Remove(key)
 	c.lock.Unlock()
@@ -111,14 +134,20 @@ func (c *Cache) Remove(key interface{}) (present bool) {
 
 // Resize changes the cache size.
 func (c *Cache) Resize(maxWeight uint, maxSize int) (evicted int) {
+	if c == nil {
+		return
+	}
 	c.lock.Lock()
 	evicted = c.lru.Resize(maxWeight, maxSize)
 	c.lock.Unlock()
-	return evicted
+	return
 }
 
 // RemoveOldest removes the oldest item from the cache.
 func (c *Cache) RemoveOldest() (key interface{}, value interface{}, ok bool) {
+	if c == nil {
+		return
+	}
 	c.lock.Lock()
 	key, value, ok = c.lru.RemoveOldest()
 	c.lock.Unlock()
@@ -127,6 +156,9 @@ func (c *Cache) RemoveOldest() (key interface{}, value interface{}, ok bool) {
 
 // GetOldest returns the oldest entry
 func (c *Cache) GetOldest() (key interface{}, value interface{}, ok bool) {
+	if c == nil {
+		return
+	}
 	c.lock.Lock()
 	key, value, ok = c.lru.GetOldest()
 	c.lock.Unlock()
@@ -135,6 +167,9 @@ func (c *Cache) GetOldest() (key interface{}, value interface{}, ok bool) {
 
 // Keys returns a slice of the keys in the cache, from oldest to newest.
 func (c *Cache) Keys() []interface{} {
+	if c == nil {
+		return nil
+	}
 	c.lock.RLock()
 	keys := c.lru.Keys()
 	c.lock.RUnlock()
@@ -143,6 +178,9 @@ func (c *Cache) Keys() []interface{} {
 
 // Len returns the number of items in the cache.
 func (c *Cache) Len() int {
+	if c == nil {
+		return 0
+	}
 	c.lock.RLock()
 	length := c.lru.Len()
 	c.lock.RUnlock()
@@ -151,6 +189,9 @@ func (c *Cache) Len() int {
 
 // Weight returns the total weight of items in the cache.
 func (c *Cache) Weight() uint {
+	if c == nil {
+		return 0
+	}
 	c.lock.RLock()
 	w := c.lru.Weight()
 	c.lock.RUnlock()
@@ -159,8 +200,11 @@ func (c *Cache) Weight() uint {
 
 // Total returns the total weight and number of items in the cache.
 func (c *Cache) Total() (weight uint, num int) {
+	if c == nil {
+		return
+	}
 	c.lock.RLock()
 	weight, num = c.lru.Total()
 	c.lock.RUnlock()
-	return weight, num
+	return
 }
