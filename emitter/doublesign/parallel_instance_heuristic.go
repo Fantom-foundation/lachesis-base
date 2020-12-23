14add85b59a785aa4537c101dfae137ee09a32e2
Egor Lysenko <uprendis@gmail.com>
2020-12-23 15:45:11 +0700

    fix prallel instance detection

--- emitter/doublesign/parallel_instance_heuristic.go
+++ emitter/doublesign/parallel_instance_heuristic.go
@@ -2,11 +2,11 @@ package doublesign
 
 import "time"
 
-// DetectParallelInstance should be called after downloading a self-event which wasn't created this node
+// DetectParallelInstance should be called after downloading a self-event which wasn't created on this instance
 // Returns true if a parallel instance is likely be running
 func DetectParallelInstance(s SyncStatus, threshold time.Duration) bool {
 	if s.ExternalSelfEventCreated.Before(s.Startup) {
 		return false
 	}
-	return s.Since(s.ExternalSelfEventCreated) >= threshold
+	return s.Since(s.ExternalSelfEventCreated) < threshold
 }
