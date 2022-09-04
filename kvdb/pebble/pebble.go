5a3e392bdecd8619284c8e456a87c4d754541040
Egor Lysenko <uprendis@gmail.com>
2022-09-04 17:16:34 +0700

    fix memory leak in Pebble cache

--- kvdb/pebble/pebble.go
+++ kvdb/pebble/pebble.go
@@ -136,12 +136,14 @@ var adjustCache = piecefunc.NewFunc([]piecefunc.Dot{
 // metrics reporting should use for surfacing internal stats.
 func New(path string, cache int, handles int, close func() error, drop func()) (*Database, error) {
 	cache = int(adjustCache(uint64(cache)))
+	ref := pebble.NewCache(int64(cache * 2 / 3))
+	defer ref.Unref()
 	db, err := pebble.Open(path, &pebble.Options{
-		Cache:                    pebble.NewCache(int64(cache * 2 / 3)), // default 8 MB
-		MemTableSize:             cache / 3,                             // default 4 MB
-		MaxOpenFiles:             handles,                               // default 1000
-		WALBytesPerSync:          0,                                     // default 0 (matches RocksDB = no background syncing)
-		MaxConcurrentCompactions: 3,                                     // default 1, important for big imports performance
+		Cache:                    ref,       // default 8 MB
+		MemTableSize:             cache / 3, // default 4 MB
+		MaxOpenFiles:             handles,   // default 1000
+		WALBytesPerSync:          0,         // default 0 (matches RocksDB = no background syncing)
+		MaxConcurrentCompactions: 3,         // default 1, important for big imports performance
 	})
 
 	if err != nil {
