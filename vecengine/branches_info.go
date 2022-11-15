23972cc0493a3f6072211b652873aff8d2526493
martin arrivets <arrivets@gmail.com>
2022-11-15 07:40:35 +0000

    Initialize empty vectorengine Events cache

--- vecengine/branches_info.go
+++ vecengine/branches_info.go
@@ -48,7 +48,7 @@ func newInitialBranchesInfo(validators *pos.Validators) *BranchesInfo {
 		branchIDByCreators[i][0] = idx.Validator(i)
 	}
 
-	events := make([]hash.Event, len(branchIDCreators)*100)
+	events := make([]hash.Event, 0, len(branchIDCreators)*100)
 
 	return &BranchesInfo{
 		BranchIDLastSeq:     branchIDLastSeq,
