8ab29bc9cd8ee686e23fe08e7bd304badff75086
martin arrivets <arrivets@gmail.com>
2023-06-12 04:16:05 +0400

    add vecflushable storage from https://github.com/arrivets/lachesis-base/tree/flashable

---
+++ vecengine/vecflushable/backed_map.go
@@ -0,0 +1,128 @@
+package vecflushable
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+
+	"github.com/Fantom-foundation/lachesis-base/kvdb"
+	"github.com/Fantom-foundation/lachesis-base/kvdb/batched"
+)
+
+// TestSizeLimit is used as the limit in unit-test of packages that use vecflushable
+const TestSizeLimit = 100000
+
+type backedMap struct {
+	cache               map[string][]byte
+	cacheIndex          []string
+	backup              kvdb.Store
+	sizeLimit           int
+	evictionThreshold   int
+	cacheSizeEstimation int
+}
+
+func newBackedMap(backup kvdb.Store, sizeLimit int, evictionThreshold int) *backedMap {
+	return &backedMap{
+		cache:             make(map[string][]byte),
+		cacheIndex:        []string{},
+		backup:            backup,
+		sizeLimit:         sizeLimit,
+		evictionThreshold: evictionThreshold,
+	}
+}
+
+func (w *backedMap) has(key []byte) (bool, error) {
+	if _, ok := w.cache[string(key)]; ok {
+		return true, nil
+	}
+	val, err := w.backup.Get(key)
+	if err != nil {
+		return false, err
+	}
+	return val != nil, nil
+}
+
+func (w *backedMap) get(key []byte) ([]byte, error) {
+	if val, ok := w.cache[string(key)]; ok {
+		return common.CopyBytes(val), nil
+	}
+	return w.backup.Get(key)
+}
+
+func (w *backedMap) close() error {
+	w.cache = nil
+	w.cacheIndex = nil
+	return w.backup.Close()
+}
+
+func (w *backedMap) add(key string, val []byte) {
+	lenBefore := len(w.cache)
+	w.cache[key] = val
+	if len(w.cache) > lenBefore {
+		w.cacheIndex = append(w.cacheIndex, key)
+		w.cacheSizeEstimation += MapConst + 2*len(key) + len(val)
+	}
+}
+
+func (w *backedMap) unloadIfNecessary() error {
+	if w.cacheSizeEstimation < w.sizeLimit {
+		return nil
+	}
+
+	batch := w.backup.NewBatch()
+	defer batch.Reset()
+
+	cutoff := 0
+	removedEstimation := 0
+	for _, key := range w.cacheIndex {
+		var err error
+
+		val, ok := w.cache[key]
+		if !ok {
+			return errInconsistent
+		}
+
+		err = batch.Put([]byte(key), val)
+		if err != nil {
+			return err
+		}
+
+		if batch.ValueSize() > kvdb.IdealBatchSize {
+			err = batch.Write()
+			if err != nil {
+				return err
+			}
+			batch.Reset()
+		}
+
+		delete(w.cache, key)
+		removedEstimation += (2*len(key) + len(val))
+		cutoff++
+
+		if removedEstimation >= w.evictionThreshold {
+			break
+		}
+	}
+
+	err := batch.Write()
+	if err != nil {
+		return err
+	}
+
+	w.cacheIndex = w.cacheIndex[cutoff:]
+	w.cacheSizeEstimation -= removedEstimation
+
+	return nil
+}
+
+func (w *backedMap) deleteAll() {
+	w.cache = make(map[string][]byte)
+	w.cacheIndex = []string{}
+	w.cacheSizeEstimation = 0
+
+	wrappedDB := batched.Wrap(w.backup)
+	it := wrappedDB.NewIterator(nil, nil)
+	for it.Next() {
+		wrappedDB.Delete(it.Key())
+	}
+	it.Release()
+	wrappedDB.Flush()
+}
---
+++ vecengine/vecflushable/vecflushable.go
@@ -0,0 +1,159 @@
+package vecflushable
+
+import (
+	"errors"
+
+	"github.com/Fantom-foundation/lachesis-base/kvdb"
+	"github.com/ethereum/go-ethereum/common"
+)
+
+var (
+	errClosed         = errors.New("vecflushable - database closed")
+	errNotImplemented = errors.New("vecflushable - not implemented")
+	errInconsistent   = errors.New("vecflushable - inconsistent db")
+)
+
+// MapConst is an approximation of the number of extra bytes used by native go
+// maps when adding an item to a map.
+const MapConst = 100
+
+// VecFlushable is a fast, append only, Flushable intended for the vecengine.
+// It does not implement all of the Flushable interface, just what is needed by
+// the vecengine.
+type VecFlushable struct {
+	modified       map[string][]byte
+	underlying     backedMap
+	sizeEstimation int
+	onDrop         func()
+}
+
+func Wrap(parent kvdb.Store, sizeLimit int) *VecFlushable {
+	if parent == nil {
+		panic("nil parent")
+	}
+	return WrapWithDrop(parent, sizeLimit, parent.Drop)
+}
+
+func WrapWithDrop(parent kvdb.Store, sizeLimit int, drop func()) *VecFlushable {
+	if parent == nil {
+		panic("nil parent")
+	}
+	return &VecFlushable{
+		modified:   make(map[string][]byte),
+		underlying: *newBackedMap(parent, sizeLimit, sizeLimit/2),
+		onDrop:     drop,
+	}
+}
+
+func (w *VecFlushable) clearModified() {
+	w.modified = make(map[string][]byte)
+	w.sizeEstimation = 0
+}
+
+func (w *VecFlushable) Has(key []byte) (bool, error) {
+	if w.modified == nil {
+		return false, errClosed
+	}
+	_, ok := w.modified[string(key)]
+	if ok {
+		return true, nil
+	}
+	return w.underlying.has(key)
+}
+
+func (w *VecFlushable) Get(key []byte) ([]byte, error) {
+	if w.modified == nil {
+		return nil, errClosed
+	}
+	if val, ok := w.modified[string(key)]; ok {
+		return common.CopyBytes(val), nil
+	}
+	return w.underlying.get(key)
+}
+
+func (w *VecFlushable) Put(key []byte, value []byte) error {
+	if value == nil || key == nil {
+		return errors.New("vecflushable: key or value is nil")
+	}
+	w.modified[string(key)] = common.CopyBytes(value)
+	w.sizeEstimation += MapConst + len(key) + len(value)
+	return nil
+}
+
+func (w *VecFlushable) NotFlushedPairs() int {
+	return len(w.modified)
+}
+
+func (w *VecFlushable) NotFlushedSizeEst() int {
+	return w.sizeEstimation
+}
+
+func (w *VecFlushable) Flush() error {
+	if w.modified == nil {
+		return errClosed
+	}
+
+	w.underlying.unloadIfNecessary()
+
+	for key, val := range w.modified {
+		w.underlying.add(key, val)
+	}
+
+	w.clearModified()
+
+	return nil
+}
+
+func (w *VecFlushable) DropNotFlushed() {
+	w.clearModified()
+}
+
+func (w *VecFlushable) DeleteAll() {
+	w.DropNotFlushed()
+	w.underlying.deleteAll()
+}
+
+func (w *VecFlushable) Close() error {
+	if w.modified == nil {
+		return errClosed
+	}
+	w.DropNotFlushed()
+	w.modified = nil
+	return w.underlying.close()
+}
+
+func (w *VecFlushable) Drop() {
+	if w.modified != nil {
+		panic("close db first")
+	}
+	if w.onDrop != nil {
+		w.onDrop()
+	}
+}
+
+/* Some methods are not implemented and panic when called */
+
+func (w *VecFlushable) Delete(key []byte) error {
+	panic(errNotImplemented)
+}
+
+func (w *VecFlushable) GetSnapshot() (kvdb.Snapshot, error) {
+	panic(errNotImplemented)
+}
+
+func (w *VecFlushable) NewIterator(prefix []byte, start []byte) kvdb.Iterator {
+	panic(errNotImplemented)
+}
+
+func (w *VecFlushable) Stat(property string) (string, error) {
+	panic(errNotImplemented)
+}
+
+func (w *VecFlushable) Compact(start []byte, limit []byte) error {
+	panic(errNotImplemented)
+}
+
+func (w *VecFlushable) NewBatch() kvdb.Batch {
+	panic(errNotImplemented)
+}
+
